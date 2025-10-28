f04d7eb0bdba33b8b8134e23a48045e5f3a12c28
Mats Åkebrand <malle@akebrands.se>
2025-10-28 10:23:56 +0100

    refactor form errors

--- src/Support/Validator.php
+++ src/Support/Validator.php
@@ -180,7 +180,7 @@ class Validator
             'ip' => "Fältet $translatedField måste vara en giltig IP-adress.",
             'url' => "Fältet $translatedField måste vara en giltig URL.",
 //            'required_with' => "Fältet $translatedField är obligatoriskt när $translatedParameter anges.",
-            'required_with' => "Fältet $translatedField är obligatoriskt med $translatedParameter.",
+            'required_with' => "Fältet $translatedField krävs när $translatedParameter anges.",
             'nullable' => "Fältet $translatedField får lämnas tomt, men om det anges måste det uppfylla valideringsreglerna.",
             'string' => "Fältet $translatedField måste vara en giltig textsträng.", // Lägg till detta
             'file_type' => "Fältet $translatedField måste vara av typen: $parameter.",
