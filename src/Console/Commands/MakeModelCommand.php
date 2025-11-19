9363e34aeeeb0360ef30bf6c2a0510debc502468
Mats Åkebrand <malle@akebrands.se>
2025-11-19 18:46:12 +0100

    PHP-CS-Fixer

--- src/Console/Commands/MakeModelCommand.php
+++ src/Console/Commands/MakeModelCommand.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Radix\Console\Commands;
 
-use Radix\Support\StringHelper; // använd relationernas pluralizer
+use Radix\Support\StringHelper;
+
+ // använd relationernas pluralizer
 
 class MakeModelCommand extends BaseCommand
 {
